970f1fa9dce230cceed4b52b64ff7748b278cafe
RumitOsu <103869313+RumitOsu@users.noreply.github.com>
2024-01-17 18:29:50 -0700

    added comments

--- DiceRoller.cs
+++ DiceRoller.cs
@@ -1,24 +1,32 @@
 ﻿using System;
 
-namespace DiceRollSimulator
+namespace DiceRollSim
 {
-    public class DiceRoller
+    // class for rolling the dice
+    public class DiceRoll
     {
+        //random number generator
         private Random random = new Random();
-
-        public int[] RollDices(int numberOfRolls)
+        
+        // rolling the dices user input amount of times
+        public int[] RollDices(int iRolls)
         {
+            // track score with an array with 11 slots for each result (2 - 12).
             int[] results = new int[11];
 
-            for (int i = 0; i < numberOfRolls; i++)
+            // loop through amount of times dice needs to be rolled 
+            for (int i = 0; i < iRolls; i++)
             {
+                // rolling two dices and adding the results
                 int rollOne = random.Next(1, 7);
                 int rollTwo = random.Next(1, 7);
                 int sum = rollOne + rollTwo;
-
+                
+                // matching the results to the array slots
                 results[sum - 2]++;
             }
 
+            // returning the array
             return results;
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,28 +1,33 @@
 ﻿using System;
 
-namespace DiceRollSimulator
+namespace DiceRollSim
 {
+    // Main class
     class Program
-    {
+    {   
         static void Main(string[] args)
         {
+            // Asking for the number of rolls from the user and saving it
             Console.WriteLine("Welcome to the dice throwing simulator!");
             Console.Write("How many dice rolls would you like to simulate? ");
-            int numberOfRolls = Convert.ToInt32(Console.ReadLine());
+            int iRolls = Convert.ToInt32(Console.ReadLine());
 
-            DiceRoller roller = new DiceRoller();
-            int[] results = roller.RollDices(numberOfRolls);
+            // Creating an instance of the DiceRoll class then getting the results from it
+            DiceRoll roller = new DiceRoll();
+            int[] results = roller.RollDices(iRolls);
 
+            // Display the results in console
             Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
             Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
-            Console.WriteLine($"Total number of rolls = {numberOfRolls}.");
+            Console.WriteLine($"Total number of rolls = {iRolls}.");
 
+            // looping through the results and creating a histogram
             for (int i = 2; i <= 12; i++)
             {
                 Console.Write($"{i}: ");
-                Console.WriteLine(new string('*', results[i - 2] * 100 / numberOfRolls));
+                Console.WriteLine(new string('*', results[i - 2] * 100 / iRolls));
             }
-
+            // final message before exiting
             Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
         }
     }
