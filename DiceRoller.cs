73989b47a795231841b1a09c1a37fa0c4ae61f35
RumitOsu <103869313+RumitOsu@users.noreply.github.com>
2024-01-17 18:11:01 -0700

    Add project files.

---
+++ DiceRoller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DiceRollSimulator
+{
+    public class DiceRoller
+    {
+        private Random random = new Random();
+
+        public int[] RollDices(int numberOfRolls)
+        {
+            int[] results = new int[11];
+
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                int rollOne = random.Next(1, 7);
+                int rollTwo = random.Next(1, 7);
+                int sum = rollOne + rollTwo;
+
+                results[sum - 2]++;
+            }
+
+            return results;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DiceRollSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+            Console.Write("How many dice rolls would you like to simulate? ");
+            int numberOfRolls = Convert.ToInt32(Console.ReadLine());
+
+            DiceRoller roller = new DiceRoller();
+            int[] results = roller.RollDices(numberOfRolls);
+
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+            Console.WriteLine($"Total number of rolls = {numberOfRolls}.");
+
+            for (int i = 2; i <= 12; i++)
+            {
+                Console.Write($"{i}: ");
+                Console.WriteLine(new string('*', results[i - 2] * 100 / numberOfRolls));
+            }
+
+            Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+        }
+    }
+}
+
